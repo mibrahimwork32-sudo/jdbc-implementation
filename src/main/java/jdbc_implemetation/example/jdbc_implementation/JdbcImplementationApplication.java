9840086b2ac37e70261bf2d60403e051f626f4e4
Muhammad Ibrahim <m.ibrahim.work32@gmail.com>
2026-09-23 13:24:21 +0500

    added docker file

--- src/main/java/jdbc_implemetation/example/jdbc_implementation/JdbcImplementationApplication.java
+++ src/main/java/jdbc_implemetation/example/jdbc_implementation/JdbcImplementationApplication.java
@@ -1,29 +1,16 @@
 package jdbc_implemetation.example.jdbc_implementation;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JdbcImplementationApplication
-		extends SpringBootServletInitializer {
+public class JdbcImplementationApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(
 				JdbcImplementationApplication.class,
 				args
-		);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder application) {
-
-		return application.sources(
-				JdbcImplementationApplication.class
 		);
 	}
 }
